0c2a69314813f17d56980d524db6176c370856db
Omar Rahman <omar@r4men.com>
2025-12-02 19:36:56 -0800

    Update ModRecipeProvider.java

--- src/main/java/com/r4men/create_cobblemon/datagen/ModRecipeProvider.java
+++ src/main/java/com/r4men/create_cobblemon/datagen/ModRecipeProvider.java
@@ -1,8 +1,13 @@
 package com.r4men.create_cobblemon.datagen;
 
 import com.cobblemon.mod.common.CobblemonItems;
+import com.r4men.create_cobblemon.datagen.create.ModCrushingRecipeGen;
 import com.r4men.create_cobblemon.util.ModTags;
+import com.simibubi.create.api.data.recipe.ProcessingRecipeGen;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
@@ -10,11 +15,18 @@ import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.neoforged.neoforge.fluids.FluidType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    static final List<ProcessingRecipeGen<?, ?, ?>> GENERATORS = new ArrayList<>();
+    static final int BUCKET = FluidType.BUCKET_VOLUME;
+    static final int BOTTLE = 250;
+
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries);
     }
@@ -26,5 +38,23 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .requires(Items.BLACK_DYE)
                 .unlockedBy("has_black_dye", has(Items.BLACK_DYE))
                 .save(recipeOutput);
+    }
+
+    public static void registerAllProcessing(DataGenerator gen, PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        GENERATORS.add(new ModCrushingRecipeGen(output, registries));
+
+        gen.addProvider(true, new DataProvider() {
+            @Override
+            public @NotNull String getName() {
+                return "Create: Cobblemon Industrialized Processing Recipes";
+            }
+
+            @Override
+            public @NotNull CompletableFuture<?> run(@NotNull CachedOutput cachedOutput) {
+                return CompletableFuture.allOf(GENERATORS.stream()
+                        .map(gen -> gen.run(cachedOutput))
+                        .toArray(CompletableFuture[]::new));
+            }
+        });
     }
 }
