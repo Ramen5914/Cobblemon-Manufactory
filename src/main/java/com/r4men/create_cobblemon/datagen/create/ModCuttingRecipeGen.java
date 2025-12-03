d014df6faad6423784bbc8fb4fa02f3c279d000d
Omar Rahman <omar@r4men.com>
2025-12-03 02:43:00 -0800

    Added all cutting recipes

--- src/main/java/com/r4men/create_cobblemon/datagen/create/ModCuttingRecipeGen.java
+++ src/main/java/com/r4men/create_cobblemon/datagen/create/ModCuttingRecipeGen.java
@@ -1,13 +1,21 @@
 package com.r4men.create_cobblemon.datagen.create;
 
 import com.r4men.create_cobblemon.CreateCobblemon;
+import com.r4men.create_cobblemon.item.ModItems;
 import com.simibubi.create.api.data.recipe.CuttingRecipeGen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ModCuttingRecipeGen extends CuttingRecipeGen {
+    GeneratedRecipe IRON_BALL_BASE = create(
+            "iron_ball_base", b -> b
+                    .require(Items.IRON_INGOT)
+                    .duration(200)
+                    .output(ModItems.IRON_BALL_BASE));
+
     public ModCuttingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, CreateCobblemon.MOD_ID);
     }
