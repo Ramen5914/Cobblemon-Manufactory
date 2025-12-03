83860516533864105b905bc945bc94b0045663de
Omar Rahman <omar@r4men.com>
2025-12-03 05:34:49 -0800

    Refactor fluid registration for multiple fluid types
    Replaced single antidote fluid registration with a generic method to register multiple fluid types and their properties. This improves scalability and maintainability by supporting various potion and healing fluids in both ModFluidTypes and ModFluids.

--- src/main/java/com/r4men/create_cobblemon/fluid/ModFluidTypes.java
+++ src/main/java/com/r4men/create_cobblemon/fluid/ModFluidTypes.java
@@ -18,13 +18,32 @@ public class ModFluidTypes {
     public static final DeferredRegister<FluidType> FLUID_TYPES =
             DeferredRegister.create(NeoForgeRegistries.Keys.FLUID_TYPES, CreateCobblemon.MOD_ID);
 
-    public static final Supplier<FluidType> ANTIDOTE_WATER_FLUID_TYPE = registerFluidType("antidote_fluid_type",
-            new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, WATER_OVERLAY_RL, 0xA1FFFA8D,
-                    new Vector3f(1f, 250f / 255f, 141f / 255f),
-                    FluidType.Properties.create()));
+    public static final Supplier<FluidType> ANTIDOTE_FLUID_TYPE = registerSimpleFluidType("antidote", 0xFFFA8D);
+    public static final Supplier<FluidType> AWAKENING_FLUID_TYPE = registerSimpleFluidType("awakening", 0x92FAFF);
+    public static final Supplier<FluidType> BURN_HEAL_FLUID_TYPE = registerSimpleFluidType("burn_heal", 0x97FFB8);
+    public static final Supplier<FluidType> ELIXIR_FLUID_TYPE = registerSimpleFluidType("elixir", 0xF1B097);
+    public static final Supplier<FluidType> ETHER_FLUID_TYPE = registerSimpleFluidType("ether", 0xD8A8E0);
+    public static final Supplier<FluidType> FULL_HEAL_FLUID_TYPE = registerSimpleFluidType("full_heal", 0xF7EF3F);
+    public static final Supplier<FluidType> FULL_RESTORE_FLUID_TYPE = registerSimpleFluidType("full_restore", 0x9BD77F);
+    public static final Supplier<FluidType> HYPER_POTION_FLUID_TYPE = registerSimpleFluidType("hyper_potion", 0xF59BDD);
+    public static final Supplier<FluidType> ICE_HEAL_FLUID_TYPE = registerSimpleFluidType("ice_heal", 0xFFD0CC);
+    public static final Supplier<FluidType> MAX_ELIXIR_FLUID_TYPE = registerSimpleFluidType("max_elixir", 0x97F1E4);
+    public static final Supplier<FluidType> MAX_ETHER_FLUID_TYPE = registerSimpleFluidType("max_ether", 0xB9F197);
+    public static final Supplier<FluidType> MAX_POTION_FLUID_TYPE = registerSimpleFluidType("max_potion", 0x869EEE);
+    public static final Supplier<FluidType> MEDICINAL_BREW_FLUID_TYPE = registerSimpleFluidType("medicinal_brew", 0x2EBCA2);
+    public static final Supplier<FluidType> PARALYZE_HEAL_FLUID_TYPE = registerSimpleFluidType("paralyze_heal", 0xEDFF7A);
+    public static final Supplier<FluidType> POTION_FLUID_TYPE = registerSimpleFluidType("potion", 0xAB90CE);
+    public static final Supplier<FluidType> SUPER_POTION_FLUID_TYPE = registerSimpleFluidType("super_potion", 0xE1978D);
 
-    private static Supplier<FluidType> registerFluidType(String name, FluidType fluidType) {
-        return FLUID_TYPES.register(name, () -> fluidType);
+    private static Supplier<FluidType> registerSimpleFluidType(String name, int color) {
+        float red = ((color >> 16) & 0xFF) / 255f;
+        float green = ((color >> 8) & 0xFF) / 255f;
+        float blue = (color & 0xFF) / 255f;
+
+        return FLUID_TYPES.register(name + "_fluid_type",
+                () -> new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, WATER_OVERLAY_RL, 0xA1000000 + color,
+                        new Vector3f(red, green, blue),
+                        FluidType.Properties.create()));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/r4men/create_cobblemon/fluid/ModFluids.java
+++ src/main/java/com/r4men/create_cobblemon/fluid/ModFluids.java
@@ -13,15 +13,35 @@ import java.util.function.Supplier;
 public class ModFluids {
     public static DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(BuiltInRegistries.FLUID, CreateCobblemon.MOD_ID);
 
-    public static final Supplier<FlowingFluid> SOURCE_ANTIDOTE_WATER = FLUIDS.register("source_antidote_water",
-            () -> new BaseFlowingFluid.Source(ModFluids.ANTIDOTE_WATER_PROPERTIES));
-    public static final Supplier<FlowingFluid> FLOWING_ANTIDOTE_WATER = FLUIDS.register("flowing_antidote_water",
-            () -> new BaseFlowingFluid.Flowing(ModFluids.ANTIDOTE_WATER_PROPERTIES));
+    public static final Supplier<FlowingFluid> ANTIDOTE = createFluid("antidote", ModFluidTypes.ANTIDOTE_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> AWAKENING = createFluid("awakening", ModFluidTypes.AWAKENING_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> BURN_HEAL = createFluid("burn_heal", ModFluidTypes.BURN_HEAL_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> ELIXIR = createFluid("elixir", ModFluidTypes.ELIXIR_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> ETHER = createFluid("ether", ModFluidTypes.ETHER_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> FULL_HEAL = createFluid("full_heal", ModFluidTypes.FULL_HEAL_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> FULL_RESTORE = createFluid("full_restore", ModFluidTypes.FULL_RESTORE_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> HYPER_POTION = createFluid("hyper_potion", ModFluidTypes.HYPER_POTION_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> ICE_HEAL = createFluid("ice_heal", ModFluidTypes.ICE_HEAL_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> MAX_ELIXIR = createFluid("max_elixir", ModFluidTypes.MAX_ELIXIR_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> MAX_ETHER = createFluid("max_ether", ModFluidTypes.MAX_ETHER_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> MAX_POTION = createFluid("max_potion", ModFluidTypes.MAX_POTION_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> MEDICINAL_BREW = createFluid("medicinal_brew", ModFluidTypes.MEDICINAL_BREW_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> PARALYZE_HEAL = createFluid("paralyze_heal", ModFluidTypes.PARALYZE_HEAL_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> POTION = createFluid("potion", ModFluidTypes.POTION_FLUID_TYPE);
+    public static final Supplier<FlowingFluid> SUPER_POTION = createFluid("super_potion", ModFluidTypes.SUPER_POTION_FLUID_TYPE);
 
-    public static final BaseFlowingFluid.Properties ANTIDOTE_WATER_PROPERTIES = new BaseFlowingFluid.Properties(
-            ModFluidTypes.ANTIDOTE_WATER_FLUID_TYPE, SOURCE_ANTIDOTE_WATER, FLOWING_ANTIDOTE_WATER)
-            .slopeFindDistance(2)
-            .levelDecreasePerBlock(1);
+    private static Supplier<FlowingFluid> createFluid(String name, Supplier<? extends net.neoforged.neoforge.fluids.FluidType> fluidType) {
+        BaseFlowingFluid.Properties[] propertiesHolder = new BaseFlowingFluid.Properties[1];
+
+        Supplier<FlowingFluid> source = FLUIDS.register("source_" + name,
+                () -> new BaseFlowingFluid.Source(propertiesHolder[0]));
+        Supplier<FlowingFluid> flowing = FLUIDS.register("flowing_" + name,
+                () -> new BaseFlowingFluid.Flowing(propertiesHolder[0]));
+
+        propertiesHolder[0] = new BaseFlowingFluid.Properties(fluidType, source, flowing);
+
+        return source;
+    }
 
     public static void register(IEventBus eventBus) {
         FLUIDS.register(eventBus);
