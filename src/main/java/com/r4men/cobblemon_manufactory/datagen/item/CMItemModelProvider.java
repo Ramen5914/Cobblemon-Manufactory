b5a256ce86b8d49877bad1ab9fad7a7ea6b93ab8
Omar Rahman <omar@r4men.com>
2025-12-15 08:43:03 -0800

    Add model override for incomplete poke ball item
    Introduces a model override for the INCOMPLETE_POKE_BALL item based on the STEP data component predicate. Also registers the 'incomplete_poke_ball_1' item model.

--- src/main/java/com/r4men/cobblemon_manufactory/datagen/item/CMItemModelProvider.java
+++ src/main/java/com/r4men/cobblemon_manufactory/datagen/item/CMItemModelProvider.java
@@ -1,9 +1,12 @@
 package com.r4men.cobblemon_manufactory.datagen.item;
 
 import com.r4men.cobblemon_manufactory.CobblemonManufactory;
+import com.r4men.cobblemon_manufactory.component.CMDataComponentTypes;
 import com.r4men.cobblemon_manufactory.item.CMItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
@@ -17,5 +20,13 @@ public class CMItemModelProvider extends ItemModelProvider {
         for (var item : CMItems.ALL_ITEMS.stream().map(DeferredHolder::get).toList()) {
             basicItem(item);
         }
+
+        basicItem(CMItems.INCOMPLETE_POKE_BALL.get())
+                .override()
+                .predicate(CMDataComponentTypes.STEP.getId(), 1f)
+                .model(new ModelFile.UncheckedModelFile(modLoc("item/incomplete_poke_ball_1")))
+                .end();
+
+        basicItem(ResourceLocation.fromNamespaceAndPath(CobblemonManufactory.ID, "incomplete_poke_ball_1"));
     }
 }
