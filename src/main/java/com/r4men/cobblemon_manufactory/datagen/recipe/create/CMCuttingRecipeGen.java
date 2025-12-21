9f5f72e4e1b3192cbb259445b2574537a9cef60e
Omar Rahman <omar@r4men.com>
2025-12-21 08:10:49 -0800

    Add new ball bases
    Add gold and diamond ball bases and switch to using Cobblemon's item tags for ball base creation.

--- src/main/java/com/r4men/cobblemon_manufactory/datagen/recipe/create/CMCuttingRecipeGen.java
+++ src/main/java/com/r4men/cobblemon_manufactory/datagen/recipe/create/CMCuttingRecipeGen.java
@@ -1,27 +1,39 @@
 package com.r4men.cobblemon_manufactory.datagen.recipe.create;
 
+import com.cobblemon.mod.common.api.tags.CobblemonItemTags;
 import com.r4men.cobblemon_manufactory.CobblemonManufactory;
 import com.r4men.cobblemon_manufactory.item.CMItems;
 import com.simibubi.create.api.data.recipe.CuttingRecipeGen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 public final class CMCuttingRecipeGen extends CuttingRecipeGen {
+    GeneratedRecipe COPPER_BALL_BASE = create(
+            "copper_ball_base", b -> b
+                    .require(CobblemonItemTags.TIER_1_POKE_BALL_MATERIALS)
+                    .duration(200)
+                    .output(CMItems.COPPER_BALL_BASE, 4));
+
     GeneratedRecipe IRON_BALL_BASE = create(
             "iron_ball_base", b -> b
-                    .require(Items.IRON_INGOT)
+                    .require(CobblemonItemTags.TIER_2_POKE_BALL_MATERIALS)
                     .duration(200)
                     .output(CMItems.IRON_BALL_BASE, 6));
 
-    GeneratedRecipe COPPER_BALL_BASE = create(
-            "copper_ball_base", b -> b
-                    .require(Items.COPPER_INGOT)
+    GeneratedRecipe GOLD_BALL_BASE = create(
+            "gold_ball_base", b -> b
+                    .require(CobblemonItemTags.TIER_3_POKE_BALL_MATERIALS)
                     .duration(200)
-                    .output(CMItems.COPPER_BALL_BASE, 4));
+                    .output(CMItems.GOLD_BALL_BASE, 2));
+
+    GeneratedRecipe DIAMOND_BALL_BASE = create(
+            "diamond_ball_base", b -> b
+                    .require(CobblemonItemTags.TIER_4_POKE_BALL_MATERIALS)
+                    .duration(200)
+                    .output(CMItems.DIAMOND_BALL_BASE, 1));
 
     public CMCuttingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, CobblemonManufactory.ID);
