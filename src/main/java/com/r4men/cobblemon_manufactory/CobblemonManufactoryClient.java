ba70d3f360b038e38731a221104c75d2576e41ab
Omar Rahman <omar@r4men.com>
2025-12-24 23:39:14 -0800

    Formatting

---
+++ src/main/java/com/r4men/cobblemon_manufactory/CobblemonManufactoryClient.java
@@ -0,0 +1,11 @@
+package com.r4men.cobblemon_manufactory;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = CobblemonManufactory.ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = CobblemonManufactory.ID, value = Dist.CLIENT)
+public class CMClient {
+    public 
+}
